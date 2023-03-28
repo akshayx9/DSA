b6a8a512688fa233cd32c85a6ab8f97e28279ead
Akshay <akshaysinghx9@gmail.com>
2023-03-28 10:47:48 +0530

    initial commit

---
+++ Intro/SumOfnNumbers.java
@@ -0,0 +1,8 @@
+package Intro;
+
+public class SumOfnNumbers{
+    public static void main(String[] args) {
+        int n=3;//input
+        System.out.println("Sum of "+n+" natural numbers is "+n*(n+1)/2);//formula for calculating sum of n natural numbers
+    }
+}

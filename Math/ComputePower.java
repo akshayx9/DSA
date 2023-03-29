e642c58d766bec2900be77b9a53d9b857c5755bc
Akshay <akshaysinghx9@gmail.com>
2023-03-29 23:01:44 +0530

    seventh commit

---
+++ Math/ComputePower.java
@@ -0,0 +1,21 @@
+package Math;
+
+//Program to compute x(given number) to the power of n(given number)
+
+public class ComputePower {
+    public int power(int x, int n){
+        int res=1;
+        while(n>0){ //We are using the binary exponentation concept to implement the solution here
+            if(n%2!=0){
+                res=res*x;
+            }
+            x=x*x;
+            n=n/2;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        ComputePower cp=new ComputePower();
+        System.out.println(cp.power(2, 8));
+    }
+}

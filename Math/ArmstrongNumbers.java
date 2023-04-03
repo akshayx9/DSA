05b723f6ff64cdd78a4dbe315b7249cd3056fc5a
Akshay <akshaysinghx9@gmail.com>
2023-04-03 13:10:45 +0530

    fifteenth commit

---
+++ Math/ArmstrongNumbers.java
@@ -0,0 +1,23 @@
+package Math;
+
+public class ArmstrongNumbers {
+    public String armstrongNumber(int n){
+        int res=0;
+        int temp=n;
+        while(n>0){
+            int x=n%10;
+            res=res+x*x*x;
+            n=n/10;
+        }
+        if(res==temp){
+            return "Yes";
+        }else{
+            return "No";
+        }
+    }
+public static void main(String[] args) {
+    ArmstrongNumbers an=new ArmstrongNumbers();
+    System.out.println(an.armstrongNumber(371));   
+}
+}
+
